2ae57b7c3d44da87bcdd73007fa9b47b4e3a5be6
sunchuanwen <gouri123>
2022-03-23 15:36:13 +0800

    fix: 砍掉部分需求，目前能力有限。只保留拉取模板功能

--- template/src/pages/page1.tsx
+++
@@ -1,9 +0,0 @@
-import React, {FC} from 'react';
-import testImg from '@/assets/test.png';
-import Style from './style.module.less';
-
-
-const Page1: FC = () => {
-	return <div className={Style.Xx}><img src={testImg} alt=""/>会话11111</div>;
-};
-export default Page1;
