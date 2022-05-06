9e849313a407e77560cafb73c68a1503cb3f0ef0
sunchuanwen <gouri123>
2022-05-06 08:56:29 +0800

    fix: 添加watch配置

--- config/webpack.start.js
+++ config/webpack.start.js
@@ -9,9 +9,15 @@ module.exports =async(options)=> {
 
 	return {
 		...commonConfig,
+		watchOptions: {
+			aggregateTimeout: 600,
+			ignored: path.join(projectPath, './node_modules'),
+		},
 		mode: 'development',
 		devtool: 'eval-cheap-module-source-map', // development
 		devServer: {
+			//使用HTML5 History API时，index.html可能需要提供页面来代替任何404响应。
+			historyApiFallback: true,
 			port, //端口
 			client: {
 				progress: true, //显示进度条
