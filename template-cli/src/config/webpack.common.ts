2ee028170e58d6662be57be3fbf65cc98ca6ad20
sunchuanwen <gouri123>
2022-12-09 18:12:12 +0800

    config: 字段调整

--- template-cli/src/config/webpack.common.ts
+++ template-cli/src/config/webpack.common.ts
@@ -5,191 +5,191 @@ import chalk from 'chalk';
 import ProgressBarPlugin from 'progress-bar-webpack-plugin';
 import { initFields, ProjectConfigType } from '../utils/tools';
 
-export default function ({ projectPath, entryPath, templatePath, cliConfig }: ProjectConfigType) {
-    //开发者的自定义配置
-    const custCommonConfig = cliConfig.common || {};
+export default function({ projectPath, entryPath, templatePath, cliConfig }: ProjectConfigType) {
+  //开发者的自定义配置
+  const custCommonConfig = cliConfig.common || {};
 
-    return {
-        //入口文件路径，必须为js
-        entry: entryPath,
-        //打包后文件路径
-        output: {
-            path: path.resolve(projectPath, './dist'),
-            filename: 'js/[chunkhash].[name].js',
-            // 静态文件打包后的路径及文件名（默认是走全局的，如果有独立的设置就按照自己独立的设置来。）
-            assetModuleFilename: 'assets/[name]_[chunkhash][ext]',
-            publicPath: custCommonConfig.publicPath || initFields.publicPath
+  return {
+    //入口文件路径，必须为js
+    entry: entryPath,
+    //打包后文件路径
+    output: {
+      path: path.resolve(projectPath, './dist'),
+      filename: 'js/[chunkhash].[name].js',
+      // 静态文件打包后的路径及文件名（默认是走全局的，如果有独立的设置就按照自己独立的设置来。）
+      assetModuleFilename: 'assets/[name]_[chunkhash][ext]',
+      publicPath: custCommonConfig.publicPath || initFields.publicPath
+    },
+    target: ['web', 'es5'], //webpack5默认生成es6，设置编译打包生成es5代码
+    cache: {
+      type: 'filesystem' // 使用文件缓存
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-react', '@babel/preset-env'],
+                plugins: [
+                  '@babel/plugin-transform-runtime',
+                  '@babel/plugin-proposal-class-properties'
+                ]
+              }
+            },
+            'ts-loader',
+            'eslint-loader'
+          ]
         },
-        target: ['web', 'es5'], //webpack5默认生成es6，设置编译打包生成es5代码
-        cache: {
-            type: 'filesystem' // 使用文件缓存
-        },
-        module: {
-            rules: [
+
+        {
+          oneOf: [
+            {
+              test: /\.module\.css$/,
+              use: [
+                MiniCssExtractPlugin.loader,
                 {
-                    test: /\.(tsx|ts)$/,
-                    exclude: /node_modules/,
-                    use: [
-                        {
-                            loader: 'babel-loader',
-                            options: {
-                                presets: ['@babel/preset-react', '@babel/preset-env'],
-                                plugins: [
-                                    '@babel/plugin-transform-runtime',
-                                    '@babel/plugin-proposal-class-properties'
-                                ]
-                            }
-                        },
-                        'ts-loader',
-                        'eslint-loader'
-                    ]
+                  loader: 'css-loader',
+                  options: {
+                    modules: {
+                      localIdentName: 'moduleStyle_[local]_[contenthash:8]'
+                    }
+                  }
                 },
-
                 {
-                    oneOf: [
-                        {
-                            test: /\.module\.css$/,
-                            use: [
-                                MiniCssExtractPlugin.loader,
-                                {
-                                    loader: 'css-loader',
-                                    options: {
-                                        modules: {
-                                            localIdentName: 'moduleStyle_[local]_[contenthash:8]'
-                                        }
-                                    }
-                                },
-                                {
-                                    loader: 'postcss-loader',
-                                    options: {
-                                        postcssOptions: {
-                                            plugins: [['autoprefixer']]
-                                        }
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            test: /\.css$/,
-                            use: [
-                                MiniCssExtractPlugin.loader,
-                                'css-loader',
-                                {
-                                    loader: 'postcss-loader',
-                                    options: {
-                                        postcssOptions: {
-                                            plugins: [['autoprefixer']]
-                                        }
-                                    }
-                                }
-                            ]
-                        },
-                        {
-                            test: /\.module\.less$/,
-
-                            use: [
-                                MiniCssExtractPlugin.loader,
-                                {
-                                    loader: 'css-loader',
-                                    options: {
-                                        modules: {
-                                            localIdentName: 'moduleStyle_[local]_[contenthash:8]'
-                                        }
-                                    }
-                                },
-
-                                {
-                                    loader: 'postcss-loader',
-                                    options: {
-                                        postcssOptions: {
-                                            plugins: [['autoprefixer']]
-                                        }
-                                    }
-                                },
-                                'less-loader'
-                            ]
-                        },
-                        {
-                            test: /\.less$/,
-                            use: [
-                                MiniCssExtractPlugin.loader,
-                                'css-loader',
-
-                                {
-                                    loader: 'postcss-loader',
-                                    options: {
-                                        postcssOptions: {
-                                            plugins: [['autoprefixer']]
-                                        }
-                                    }
-                                },
-                                'less-loader'
-                            ]
-                        },
-                        {
-                            test: /\.(jpg|png|gif|webp|bmp|jpeg)$/,
-                            type: 'asset', //在导出一个 data URI 和发送一个单独的文件之间自动选择
-                            generator: {
-                                filename: 'images/[name]_[hash][ext]' // 独立的配置
-                            }
-                        },
-                        // 字体文件
-                        {
-                            test: /\.(otf|eot|woff2?|ttf|svg)$/i,
-                            type: 'asset',
-                            generator: {
-                                filename: 'fonts/[name]_[hash][ext]'
-                            }
-                        },
-                        // 数据文件
-                        {
-                            test: /\.(txt|xml)$/i,
-                            type: 'asset/source'
-                        },
-                        {
-                            test: /\.html$/,
-                            loader: 'html-loader'
-                        }
-                    ]
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      plugins: [['autoprefixer']]
+                    }
+                  }
                 }
-            ]
-        },
-        resolve: {
-            extensions: ['.ts', '.tsx', '.js', '.jsx'],
-            alias: {
-                '@': path.resolve(projectPath, './src')
+              ]
+            },
+            {
+              test: /\.css$/,
+              use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      plugins: [['autoprefixer']]
+                    }
+                  }
+                }
+              ]
+            },
+            {
+              test: /\.module\.less$/,
+
+              use: [
+                MiniCssExtractPlugin.loader,
+                {
+                  loader: 'css-loader',
+                  options: {
+                    modules: {
+                      localIdentName: 'moduleStyle_[local]_[contenthash:8]'
+                    }
+                  }
+                },
+
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      plugins: [['autoprefixer']]
+                    }
+                  }
+                },
+                'less-loader'
+              ]
+            },
+            {
+              test: /\.less$/,
+              use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+
+                {
+                  loader: 'postcss-loader',
+                  options: {
+                    postcssOptions: {
+                      plugins: [['autoprefixer']]
+                    }
+                  }
+                },
+                'less-loader'
+              ]
+            },
+            {
+              test: /\.(jpg|png|gif|webp|bmp|jpeg)$/,
+              type: 'asset', //在导出一个 data URI 和发送一个单独的文件之间自动选择
+              generator: {
+                filename: 'images/[name]_[hash][ext]' // 独立的配置
+              }
+            },
+            // 字体文件
+            {
+              test: /\.(otf|eot|woff2?|ttf|svg)$/i,
+              type: 'asset',
+              generator: {
+                filename: 'fonts/[name]_[hash][ext]'
+              }
+            },
+            // 数据文件
+            {
+              test: /\.(txt|xml)$/i,
+              type: 'asset/source'
+            },
+            {
+              test: /\.html$/,
+              loader: 'html-loader'
             }
+          ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        '@': path.resolve(projectPath, './src')
+      }
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        //不使用默认html文件，使用自己定义的html模板并自动引入打包后的js/css
+        template: templatePath,
+        filename: 'index.html', //打包后的文件名
+        minify: {
+          //压缩和简化代码
+          collapseWhitespace: true, //去掉空行和空格
+          removeAttributeQuotes: true //去掉html标签属性的引号
         },
-        plugins: [
-            new HtmlWebpackPlugin({
-                //不使用默认html文件，使用自己定义的html模板并自动引入打包后的js/css
-                template: templatePath,
-                filename: 'index.html', //打包后的文件名
-                minify: {
-                    //压缩和简化代码
-                    collapseWhitespace: true, //去掉空行和空格
-                    removeAttributeQuotes: true //去掉html标签属性的引号
-                },
-                templateParameters: {
-                    routerBase: custCommonConfig.publicPath || initFields.publicPath
-                },
-                title: custCommonConfig.title || initFields.title,
-                hash: true //对html引用的js文件添加hash戳
-            }),
+        templateParameters: {
+          publicPath: custCommonConfig.publicPath || initFields.publicPath
+        },
+        title: custCommonConfig.title || initFields.title,
+        hash: true //对html引用的js文件添加hash戳
+      }),
 
-            new MiniCssExtractPlugin({
-                filename: 'css/[contenthash].[name].css'
-            }),
+      new MiniCssExtractPlugin({
+        filename: 'css/[contenthash].[name].css'
+      }),
 
-            // 进度条
-            new ProgressBarPlugin({
-                format: `building ${chalk.blue.bold(':bar')} ${chalk.green.bold(
-                    ':percent'
-                )} (:elapsed s)`,
-                clear: false
-            }),
+      // 进度条
+      new ProgressBarPlugin({
+        format: `building ${chalk.blue.bold(':bar')} ${chalk.green.bold(
+          ':percent'
+        )} (:elapsed s)`,
+        clear: false
+      }),
 
-            ...(custCommonConfig.plugins || initFields.plugins)
-        ]
-    };
+      ...(custCommonConfig.plugins || initFields.plugins)
+    ]
+  };
 }
--- template-cli/src/config/webpack.start.ts
+++ template-cli/src/config/webpack.start.ts
@@ -4,77 +4,77 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import webpack from 'webpack';
 
-export default async function (options: ProjectConfigType, open?: boolean) {
-    const { projectPath, cliConfig, templatePath } = options;
-    const commonConfig = getCommonConfig(options);
-    //获取可用端口
-    const port = await getPort();
-    //获取开发者自定义添加的脚手架的plugin配置
-    const custDevCfg = cliConfig.dev || {};
-    const custCommonCfg = cliConfig.common || {};
-    //获取自定义变量
-    const defineVars = { ...(custCommonCfg.define ?? {}), ...(custDevCfg.define ?? {}) };
-    //basic plugins
-    const basicPlugins: any[] = [
-        new HtmlWebpackPlugin({
-            //不使用默认html文件，使用自己定义的html模板并自动引入打包后的js/css
-            template: templatePath,
-            filename: 'index.html', //打包后的文件名
-            minify: {
-                //压缩和简化代码
-                collapseWhitespace: true, //去掉空行和空格
-                removeAttributeQuotes: true //去掉html标签属性的引号
-            },
-            templateParameters: {
-                routerBase:
-                    custDevCfg.publicPath ?? custCommonCfg.publicPath ?? initFields.publicPath
-            },
-            title: custDevCfg.title ?? custCommonCfg.title ?? initFields.title,
-            hash: true //对html引用的js文件添加hash戳
-        })
-    ];
-    //处理自定义变量设置
-    if (Object.keys(defineVars).length !== 0) {
-        basicPlugins.push(
-            new webpack.DefinePlugin({
-                ...defineVars
-            })
-        );
-    }
+export default async function(options: ProjectConfigType, open?: boolean) {
+  const { projectPath, cliConfig, templatePath } = options;
+  const commonConfig = getCommonConfig(options);
+  //获取可用端口
+  const port = await getPort();
+  //获取开发者自定义添加的脚手架的plugin配置
+  const custDevCfg = cliConfig.dev || {};
+  const custCommonCfg = cliConfig.common || {};
+  //获取自定义变量
+  const defineVars = { ...(custCommonCfg.define ?? {}), ...(custDevCfg.define ?? {}) };
+  //basic plugins
+  const basicPlugins: any[] = [
+    new HtmlWebpackPlugin({
+      //不使用默认html文件，使用自己定义的html模板并自动引入打包后的js/css
+      template: templatePath,
+      filename: 'index.html', //打包后的文件名
+      minify: {
+        //压缩和简化代码
+        collapseWhitespace: true, //去掉空行和空格
+        removeAttributeQuotes: true //去掉html标签属性的引号
+      },
+      templateParameters: {
+        publicPath:
+          custDevCfg.publicPath ?? custCommonCfg.publicPath ?? initFields.publicPath
+      },
+      title: custDevCfg.title ?? custCommonCfg.title ?? initFields.title,
+      hash: true //对html引用的js文件添加hash戳
+    })
+  ];
+  //处理自定义变量设置
+  if (Object.keys(defineVars).length !== 0) {
+    basicPlugins.push(
+      new webpack.DefinePlugin({
+        ...defineVars
+      })
+    );
+  }
 
-    return {
-        ...commonConfig,
-        //打包后文件路径
-        output: {
-            ...commonConfig.output,
-            publicPath: custDevCfg.publicPath ?? custCommonCfg.publicPath ?? initFields.publicPath
-        },
-        mode: 'development',
-        devtool: 'eval-cheap-module-source-map', // development
-        devServer: {
-            //使用HTML5 History API时，index.html可能需要提供页面来代替任何404响应。
-            historyApiFallback: true,
-            port, //端口
-            client: {
-                progress: true, //显示进度条
-                //警告不会显示页面
-                overlay: {
-                    errors: true,
-                    warnings: false
-                }
-            },
-            compress: true, //启动gzip压缩
-            hot: true, //热更新
-            open: open ?? true, //自动打开浏览器,
-            static: {
-                //提供静态文件服务的路径
-                directory: path.join(projectPath, './public')
-            }
-        },
-        plugins: [
-            ...basicPlugins,
-            ...commonConfig.plugins,
-            ...(custDevCfg.plugins ?? initFields.plugins)
-        ]
-    };
+  return {
+    ...commonConfig,
+    //打包后文件路径
+    output: {
+      ...commonConfig.output,
+      publicPath: custDevCfg.publicPath ?? custCommonCfg.publicPath ?? initFields.publicPath
+    },
+    mode: 'development',
+    devtool: 'eval-cheap-module-source-map', // development
+    devServer: {
+      //使用HTML5 History API时，index.html可能需要提供页面来代替任何404响应。
+      historyApiFallback: true,
+      port, //端口
+      client: {
+        progress: true, //显示进度条
+        //警告不会显示页面
+        overlay: {
+          errors: true,
+          warnings: false
+        }
+      },
+      compress: true, //启动gzip压缩
+      hot: true, //热更新
+      open: open ?? true, //自动打开浏览器,
+      static: {
+        //提供静态文件服务的路径
+        directory: path.join(projectPath, './public')
+      }
+    },
+    plugins: [
+      ...basicPlugins,
+      ...commonConfig.plugins,
+      ...(custDevCfg.plugins ?? initFields.plugins)
+    ]
+  };
 }
